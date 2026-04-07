3755ef97bf4ca82d2a231f22d2bdc5464f8287fd
Alex <oleksi.ovsiannikov@gmail.com>
2026-04-08 00:04:38 +0200

    fix count filenames starting with uppercase and no additional uppercase letters

--- shell-pipelines/ls-grep/script-04.sh
+++ shell-pipelines/ls-grep/script-04.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 # TODO: Write a command to count the number of files in the sample-files directory whose name starts with an upper case letter and doesn't contain any other upper case letters.
 # Your output should be the number 7.
-ls sample-files/[A-Z][a-z]* | wc -l
+ls -1 sample-files | grep '^[[:upper:]]' | grep -v '[A-Z].*[A-Z]' | wc -l
