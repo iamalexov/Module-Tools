de950c62e0dc72c71001579d9b4801f5e25e8df9
Alex <oleksi.ovsiannikov@gmail.com>
2026-04-07 23:35:53 +0200

    fix: output only file names

--- shell-pipelines/ls-grep/script-02.sh
+++ shell-pipelines/ls-grep/script-02.sh
@@ -5,4 +5,3 @@ set -euo pipefail
 # TODO: Write a command to output the names of the files in the sample-files directory whose name starts with an upper case letter.
 # Your output should contain 10 files.
 ls -1 sample-files | grep '^[[:upper:]]'
-ls -1 sample-files/[A-Z]*
