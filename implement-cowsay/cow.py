d82e81a16c4cf5397842a4b46ab2f86caadfbea1
Alex <oleksi.ovsiannikov@gmail.com>
2026-04-18 23:54:05 +0200

    feat: implement cowsay CLI with argparse and dynamic animal selection

--- implement-cowsay/cow.py
+++ implement-cowsay/cow.py
@@ -1,7 +1,26 @@
+import argparse
 import cowsay
-import sys
 
-words = sys.argv[1:]          
-message = " ".join(words)     
+parser = argparse.ArgumentParser(
+    prog="cowsay",
+    description="Make animals say things"
+)
 
-cowsay.cow(message)
+parser.add_argument(
+    "--animal",
+    choices=cowsay.char_names,
+    default="cow",
+    help="The animal to be saying things."
+)
+
+parser.add_argument(
+    "message",
+    nargs="+",
+    help="The message to say."
+)
+
+args = parser.parse_args()
+
+message = " ".join(args.message)
+
+getattr(cowsay, args.animal)(message)
