0517d652aa8db849f3caa038bf8a790d291da232
Alex <oleksi.ovsiannikov@gmail.com>
2026-04-20 00:10:16 +0200

    feat: implement basic ls command in Python

---
+++ implement-shell-tools/ls/my-ls.py
@@ -0,0 +1,15 @@
+import sys
+import os
+
+args = sys.argv[1:]
+
+path = args[0] if args else "."
+
+try:
+    files = os.listdir(path)
+except:
+    print(f"ls: cannot access '{path}'")
+    exit(1)
+
+for file in files:
+    print(file)
