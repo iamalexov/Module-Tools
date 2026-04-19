89d96eef0fa016e0ede60da7c79880cee3bc79f7
Alex <oleksi.ovsiannikov@gmail.com>
2026-04-20 00:06:24 +0200

    feat: implement cat CLI in Python with -n and -b options and stdin support

--- implement-shell-tools/cat/my-cat.py
+++ implement-shell-tools/cat/my-cat.py
@@ -13,6 +13,49 @@ parser.add_argument("-b", action="store_true", help="Number non-blank lines")
 
 args = parser.parse_args()
 
+
+def print_stdin():
+    line_number = 1
+    for line in sys.stdin:
+        line = line.rstrip("\n")
+
+        if args.b:
+            if line != "":
+                print(f"{line_number} {line}")
+                line_number += 1
+            else:
+                print()
+        elif args.n:
+            print(f"{line_number} {line}")
+            line_number += 1
+        else:
+            print(line)
+
+
+def print_file(file, line_number):
+    try:
+        with open(file, "r") as f:
+            for line in f:
+                line = line.rstrip("\n")
+
+                if args.b:
+                    if line != "":
+                        print(f"{line_number} {line}")
+                        line_number += 1
+                    else:
+                        print()
+                elif args.n:
+                    print(f"{line_number} {line}")
+                    line_number += 1
+                else:
+                    print(line)
+
+    except:
+        print(f"cat: {file}: No such file")
+
+    return line_number
+
+
 def main():
     line_number = 1
 
