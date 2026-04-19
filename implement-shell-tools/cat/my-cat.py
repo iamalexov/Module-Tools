51bfe3958dfea8cddd0db4fa1b47d85816a5b57d
Alex <oleksi.ovsiannikov@gmail.com>
2026-04-19 23:42:50 +0200

    feat: implement file reading and line numbering (-n, -b) for cat

--- implement-shell-tools/cat/my-cat.py
+++ implement-shell-tools/cat/my-cat.py
@@ -6,3 +6,25 @@ flag_n = "-n" in args
 flag_b = "-b" in args
 
 files = [arg for arg in args if arg not in ["-n", "-b"]]
+line_number = 1
+
+for file in files:
+    try:
+        with open(file, "r") as f:
+            lines = f.read().split("\n")
+    except:
+        print(f"cat: {file}: No such file")
+        continue
+
+    for line in lines:
+        if flag_b:
+            if line != "":
+                print(f"{line_number} {line}")
+                line_number += 1
+            else:
+                print("")
+        elif flag_n:
+            print(f"{line_number} {line}")
+            line_number += 1
+        else:
+            print(line)
