3ad5e264e865c7a29a2e2f64ccebc00987b4754e
Alex <oleksi.ovsiannikov@gmail.com>
2026-04-19 21:04:37 +0200

    feat: implement basic cat CLI to read and print files

---
+++ implement-shell-tools/cat/my-cat.mjs
@@ -0,0 +1,27 @@
+/* # Implement `cat`
+
+You should already be familiar with the `cat` command line tool.
+
+Your task is to implement your own version of `cat`.
+
+It must act the same as `cat` would, if run from the directory containing this README.md file, for the following command lines:
+
+/* `cat sample-files/1.txt`
+/* `cat -n sample-files/1.txt`
+/* `cat sample-files/*.txt`
+/* `cat -n sample-files/*.txt`
+/* `cat -b sample-files/3.txt`
+
+Matching any additional behaviours or flags are optional stretch goals.
+
+We recommend you start off supporting no flags, then add support for `-n`, then add support for `-b`.
+
+ */
+import fs from "fs";
+
+const args = process.argv.slice(2);
+
+for (const file of args) {
+  const content = fs.readFileSync(file, "utf-8");
+  console.log(content);
+}
