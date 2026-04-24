2627886d29ebc0155a1612b51274cc6ca0b7019c
Alex <oleksi.ovsiannikov@gmail.com>
2026-04-24 22:07:20 +0200

    fix: add type annotations and resolve all mypy errors

--- my_prep-exercises/02_type_checking_with_mypy/exercise_mypy.py
+++ my_prep-exercises/02_type_checking_with_mypy/exercise_mypy.py
@@ -1,31 +1,31 @@
-from typing import Dict, List
+from typing import Dict
 
 
 def open_account(balances: Dict[str, int], name: str, amount: int) -> None:
-    reveal_type(balances)
     balances[name] = amount
-def sum_balances(accounts):
+
+def sum_balances(accounts: dict[str, int]) -> int:
     total = 0
     for name, pence in accounts.items():
         print(f"{name} had balance {pence}")
         total += pence
     return total
 
-def format_pence_as_string(total_pence):
+def format_pence_as_string(total_pence: int) -> str:
     if total_pence < 100:
         return f"{total_pence}p"
-    pounds = int(total_pence / 100)
+    pounds = total_pence / 100
     pence = total_pence % 100
     return f"£{pounds}.{pence:02d}"
 
-balances = {
+balances: dict[str, int] = {
     "Sima": 700,
     "Linn": 545,
     "Georg": 831,
 }
 
-open_account("Tobi", 9.13)
-open_account("Olya", "£7.13")
+open_account(balances, "Tobi", 913)
+open_account(balances, "Olya", 713)
 
 total_pence = sum_balances(balances)
 total_string = format_pence_as_string(total_pence)
